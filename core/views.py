46537f6d5557c88b3ba86df7c95a732f69629caf
beingbiplov <beingbiplov@gmail.com>
2021-10-12 15:43:01 +0545

    add default django authentication

--- core/views.py
+++ core/views.py
@@ -1,8 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework.permissions import IsAuthenticated
 
 class IndexView(APIView):
+	permission_classes = (IsAuthenticated,)
 	def get(self, requset):
 		content = {'msg': "Hello there!! Just checking."}
 		return Response(content)
